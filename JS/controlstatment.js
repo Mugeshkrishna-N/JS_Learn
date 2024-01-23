af7eb0dd4edbb18d5492e308db60ea4abffd17a8
Mugeshkrishna <mugeshkrishna585@gmail.com>
2024-01-23 09:50:26 +0530

    control statements

---
+++ JS/controlstatment.js
@@ -0,0 +1,17 @@
+var a=10;
+var b=20;
+
+if (a>b) {
+    console.log("A is the greatest");
+  } else {
+    console.log("B is the greatest");
+  }
+  
+  for (let i = 0; i < a; i++) {
+        console.log(i);
+  }
+  
+  while (a!=0) {
+    a--;
+}
+  
